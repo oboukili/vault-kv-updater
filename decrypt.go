8442c83a73d356b4855d38302245928bbb749242
Olivier Boukili <boukili.olivier@gmail.com>
2019-08-14 20:43:25 +0200

    refactoring

--- decrypt.go
+++ decrypt.go
@@ -5,41 +5,38 @@ import (
 	"github.com/go-yaml/yaml"
 	sops "go.mozilla.org/sops/decrypt"
 	"io/ioutil"
-	"log"
+	"os"
 	"reflect"
 )
 
-func isSopsEncryptedYamlFile(path string) (bool ,error) {
-	input, err := ioutil.ReadFile(path)
+// Asserts whether the input (stdin or file) is a sops encrypted stream, returns
+func isSopsEncrypted(i interface{}) (ok bool, err error, input []byte) {
+
+	switch i.(type) {
+	case string:
+		input, err = ioutil.ReadFile(i.(string))
+	case *os.File:
+		input, err = ioutil.ReadAll(i.(*os.File))
+	}
 	if err != nil {
-		return false, fmt.Errorf("isSopsEncryptedYamlFile; %s", err)
+		err = fmt.Errorf("isSopsEncrypted; %s", err)
+		return
 	}
 
 	var contents map[interface{}]interface{}
-	if err := yaml.Unmarshal(input, &contents); err != nil {
-		return false, fmt.Errorf("isSopsEncryptedYamlFile; %s", err)
+	err = yaml.Unmarshal(input, &contents)
+	if err != nil {
+		err = fmt.Errorf("isSopsEncrypted; %s", err)
+		return
 	}
-	
-	if isSopsEncryptedData(contents) {
-		log.Printf("Detected Sops encrypted data for %s", path)
-		return true, nil
-	}
-	return false, nil
-}
 
-func isSopsEncryptedData(input interface{}) bool {
-	switch input.(type) {
-	//TODO? more readable type assertions?
-	case map[interface{}]interface{}:
-		if _, ok := input.(map[interface{}]interface{})["sops"]; ok {
-			switch input.(map[interface{}]interface{})["sops"].(type) {
-			case map[interface{}]interface{}:
-				_, ok := input.(map[interface{}]interface{})["sops"].(map[interface{}]interface{})["version"]
-				return ok
-			}
+	if _, ok = contents["sops"]; ok {
+		switch contents["sops"].(type) {
+		case map[interface{}]interface{}:
+			_, ok = contents["sops"].(map[interface{}]interface{})["version"]
 		}
 	}
-	return false
+	return
 }
 
 func decrypt(input interface{}, format string) (contents []byte, err error) {
--- main.go
+++ main.go
@@ -7,61 +7,49 @@ import (
 	"fmt"
 	"github.com/go-yaml/yaml"
 	"github.com/jeremywohl/flatten"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
-func processFile(path string) error {
-	var input []byte
-
-	ok, err := isSopsEncryptedYamlFile(path)
+func routine(i interface{}) (err error) {
+	ok, err, input := isSopsEncrypted(i)
 	if err != nil {
-		return fmt.Errorf("processFile: %s", err)
+		return
 	}
 	if ok {
-		input, err = decrypt(path, "yaml")
+		input, err = decrypt(input, "yaml")
 		if err != nil {
-			return fmt.Errorf("processFile: %s", err)
+			return
 		}
 	}
-
 	// unmarshal yaml, because we know we are working with yaml
 	var contents interface{}
 	if err := yaml.Unmarshal(input, &contents); err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	// decode yaml contents to map[string]interface{} instance
 	decodedContents, err := decode(contents)
 	if err != nil {
-		return fmt.Errorf("decode: %s", err)
+		return
 	}
 
 	content, err := json.Marshal(decodedContents)
 	if err != nil {
-		return fmt.Errorf("jsonMarshal: %s", err)
+		return
 	}
 
 	unescapedContent, err := unescapeUnicodeCharactersInJSON(json.RawMessage(content))
 	if err != nil {
-		return fmt.Errorf("unescapeUnicodeCharactersInJSON: %s", err)
+		return
 	}
 
 	if flattened, err := flatten.FlattenString(string(unescapedContent), "", flatten.DotStyle); err != nil {
-		return fmt.Errorf("flatten: %s", err)
-	} else {
-		fmt.Println(string(flattened))
-	}
-	return nil
-}
-
-func processStdin() error {
-	_, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
 		return err
+	} else {
+		fmt.Println(flattened)
 	}
-	return nil
+	return
 }
 
 func main() {
@@ -70,16 +58,17 @@ func main() {
 	if len(os.Args) > 1 {
 		for _, f := range os.Args[1:] {
 			// TODO: use a goroutine + channels for major speedups
-			err := processFile(f)
+			err := routine(f)
 			if err != nil {
 				log.Fatalln(fmt.Errorf("main: %s", err))
 			}
 		}
 	} else if stdinStat, _ := os.Stdin.Stat(); (stdinStat.Mode() & os.ModeCharDevice) == 0 {
-		if err := processStdin(); err != nil {
+		if err := routine(os.Stdin); err != nil {
 			log.Fatalln(err)
 		}
 	} else {
+		// TODO: implement a proper help message with a cli library
 		log.Fatalln(errors.New("missing input"))
 	}
 }
